f519ba0aeda823baaaf2aac4a9836a0a3d4e2aad
dimpal11 <161323087+dimpal11@users.noreply.github.com>
2024-12-25 13:10:30 +0530

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '@/components/Navbar'
+import Herosection from '@/components/Herosection'
+import Feacture from '@/components/Feacture'
+function page() {
+  return (
+    <>
+      <Navbar/>
+      <Herosection/>
+      <Feacture/>
+    </>
+  )
+}
+
+export default page
